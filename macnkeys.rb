691fe5c498fe73554e17c7322b9132af30a83220
Drew Carey Buglione <drew@drewb.ug>
2015-07-17 21:59:17 -0400

    Add initial 'configuration results file' retriever

---
+++ macnkeys.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+
+require 'test/unit/assertions'
+require 'stringio'
+
+include Test::Unit::Assertions
+
+# typedef struct {
+#   EFI_GUID                 Name;
+#   EFI_FFS_INTEGRITY_CHECK  IntegrityCheck;
+#   EFI_FV_FILETYPE          Type;
+#   EFI_FFS_FILE_ATTRIBUTES  Attributes;
+#   UINT8                    Size[3];
+#   EFI_FFS_FILE_STATE       State;
+# } EFI_FFS_FILE_HEADER;
+
+# typedef union {
+#   struct {
+#     UINT8  Header;
+#     UINT8  File;
+#   }        Checksum;
+#   UINT16   TailReference;
+# } EFI_FFS_INTEGRITY_CHECK;
+
+EFI_GUID = 'VvvC8' # UINT32, UINT16, UINT16, UINT8[8]
+EFI_FV_FILETYPE = 'C' # UINT8
+EFI_FFS_FILE_ATTRIBUTES = 'C' # UINT8
+EFI_FFS_FILE_STATE = 'C' # UINT8
+
+EFI_CONFIG_FILE_NAME_GUID = [ 0x98B8D59B, 0xE8BA, 0x48EE, 0x98, 0xDD, 0xC2, 0x95, 0x39, 0x2F, 0x1E, 0xDB ].pack(EFI_GUID)
+EFI_FV_FILETYPE_RAW = [ 0x01 ].pack(EFI_FV_FILETYPE)
+FFS_ATTRIB_CHECKSUM = [ 0x40 ].pack(EFI_FFS_FILE_ATTRIBUTES)
+
+Struct.new('File', :name, :checksum, :type, :attributes, :size, :state, :data)
+
+ARGF.binmode
+scap = StringIO.new(ARGF.file.read)
+scap.pos = scap.string.index(EFI_CONFIG_FILE_NAME_GUID)
+
+config = Struct::File.new
+
+config.name = scap.read(16)
+assert_equal EFI_CONFIG_FILE_NAME_GUID, config.name
+
+config.checksum = scap.read(2)
+
+config.type = scap.read(1)
+assert_equal EFI_FV_FILETYPE_RAW, config.type
+
+config.attributes = scap.read(1)
+assert_equal FFS_ATTRIB_CHECKSUM, config.attributes
+
+config.size = scap.read(3)
+
+config.state = scap.read(1)
+
+count = config.size.unpack('C3')
+count = count.pop << 16 | count.pop << 8 | count.pop
+config.data = scap.read(count - 24)
+assert_equal "\0\0", config.data.slice(-2, 2)
+
+puts config.data.strip
